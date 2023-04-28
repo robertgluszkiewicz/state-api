971349af121b9164bc4b03801a73772eced6edc2
Robert Gluszkiewicz <robert.glu@icloud.com>
2023-04-28 06:50:39 +0200

    Initial commit

---
+++ src/test/java/com/robertgluszkiewicz/stateapi/StateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertgluszkiewicz.stateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
